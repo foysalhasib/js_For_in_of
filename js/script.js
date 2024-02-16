2c7fd6bd6083d08f75522ce96bbd6dd1f89e26c2
Foysal Hasib <118272136+foysalhasib@users.noreply.github.com>
2024-02-17 03:04:46 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,64 @@
+//=========================== For_in(string)  ========================================
+                        //in means index
+
+
+var Name="hasib"
+
+for(var x in Name){
+     console.log(x)
+}
+
+
+
+//=========================== For_of(string)  ========================================
+
+// var Name="hasib"
+
+// for(var x of Name){
+//      console.log(x)
+// }
+
+
+//=========================== For_in(Array)  ========================================
+// var country =["Bangladesh","india","nepal","brazil"]
+
+// for(var y in country){
+//     console.log(y)
+// }
+
+//=========================== For_of(Array)  ========================================
+
+// var country =["Bangladesh","india","nepal","brazil"]
+
+// for(var y of country){
+//     console.log(y)
+// }
+
+
+//=========================== For_in(object)  ========================================
+// var person = {
+//     Name:"Foysal hasib",
+//     age:24,
+//     village:"mirpur"
+// }
+
+// for(var z in person){
+//          console.log(z)
+//      }
+
+
+
+
+//=========================== For_of(object)  ========================================
+                        //of_loop don't work object
+
+                        
+// var person = {
+//     Name:"Foysal hasib",
+//     age:24,
+//     village:"mirpur",
+// }
+
+// for(var z of person){
+//          console.log(z)
+//      }
